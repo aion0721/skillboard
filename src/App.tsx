a4e7d619256c0dc6359bc9ecd5b557450715c831
aion0721 <rudolf.aion@gmail.com>
2022-10-20 23:14:53 +0900

    Create Table

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,156 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Skills } from "./types/Skill";
+import {
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  ChakraProvider,
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
 
 function App() {
-  const targetUser: Skills = {
-    level1: [
-      {
-        name: "level1",
-        level2: [
-          {
-            name: "level2",
-            level3: [
-              { name: "testa", level: 2, content: "a" },
-              { name: "testa", level: 2, content: "a" },
-            ],
-          },
-        ],
-      },
-    ],
-  };
+  const targetUser: Array<Skills> = [
+    {
+      level1: "Zoo",
+      level2: "react",
+      level3: "Recoil",
+      level: 1,
+      content: "recoil is statement library",
+    },
+    {
+      level1: "App",
+      level2: "zoo",
+      level3: "zoo",
+      level: 1,
+      content: "recoil is statement library",
+    },
+    {
+      level1: "App",
+      level2: "zoo",
+      level3: "Recoil",
+      level: 1,
+      content: "recoil is statement library",
+    },
+    {
+      level1: "App",
+      level2: "react",
+      level3: "Recoil",
+      level: 1,
+      content: "recoil is statement library",
+    },
+  ];
+
+  const sortedTargetUser = targetUser.sort((a, b) => {
+    if (a.level1 == b.level1) {
+      if (a.level2 == b.level2) {
+        if (a.level3 > b.level3) {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        if (a.level2 > b.level2) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    } else {
+      if (a.level1 > b.level1) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  });
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div className="App">
-      {targetUser.level1.map((level1) =>
-        level1.level2.map((level2) =>
-          level2.level3.map((skill) => (
-            <p>
-              {level1.name}-{level2.name}-{skill.name}
-            </p>
-          ))
-        )
-      )}
+      <ChakraProvider>
+        <Button onClick={onOpen}>Open Modal</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>aaa</ModalBody>
+
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+              <Button>Test</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        <TableContainer>
+          <Table variant="simple">
+            <TableCaption>Sample Table</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>id</Th>
+                <Th>level1</Th>
+                <Th>level2</Th>
+                <Th>level3</Th>
+                <Th>level</Th>
+                <Th>content</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {sortedTargetUser.map((skill, index) => {
+                return (
+                  <Tr key={index}>
+                    <Td>{index}</Td>
+                    <Td>
+                      <div
+                        className={
+                          index !== 0
+                            ? sortedTargetUser[index - 1].level1 == skill.level1
+                              ? "noDisplay"
+                              : "display"
+                            : "display"
+                        }
+                      >
+                        {skill.level1}
+                      </div>
+                    </Td>
+                    <Td>
+                      <div
+                        className={
+                          index !== 0
+                            ? sortedTargetUser[index - 1].level2 == skill.level2
+                              ? "noDisplay"
+                              : "display"
+                            : "display"
+                        }
+                      >
+                        {skill.level2}
+                      </div>
+                    </Td>
+                    <Td>{skill.level3}</Td>
+                    <Td>{skill.level}</Td>
+                    <Td>{skill.content}</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </ChakraProvider>
     </div>
   );
 }
--- src/types/Skill.ts
+++ src/types/Skill.ts
@@ -1,63 +1,7 @@
 export type Skills = {
-  level1: Array<level1>;
-};
-
-type level1 = {
-  name: string;
-  level2: Array<level2>;
-};
-
-type level2 = {
-  name: string;
-  level3: Array<skillItem>;
-};
-
-type skillItem = {
-  name: string;
+  level1: string;
+  level2: string;
+  level3: string;
   level: number;
   content: string;
-};
-
-const UserA: skillItem = {
-  name: "test",
-  level: 1,
-  content: "aaa",
-};
-
-const UserB: level2 = {
-  name: "test",
-  level3: [
-    { name: "testa", level: 2, content: "a" },
-    { name: "testa", level: 2, content: "a" },
-  ],
-};
-
-const UserC: level1 = {
-  name: "test",
-  level2: [
-    {
-      name: "test",
-      level3: [
-        { name: "testa", level: 2, content: "a" },
-        { name: "testa", level: 2, content: "a" },
-      ],
-    },
-  ],
-};
-
-const targetUser: Skills = {
-  level1: [
-    {
-      name: "test",
-      level2: [
-        {
-          name: "test",
-          level3: [
-            { name: "testa", level: 2, content: "a" },
-            { name: "testa", level: 2, content: "a" },
-          ],
-        },
-      ],
-    },
-  ],
 };
